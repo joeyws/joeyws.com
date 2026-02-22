fbde72ce970962484022f40e2664333fe942c9a6
joeyws <joey.schweickhardt@gmail.com>
2026-02-22 12:01:51 +0100

    Update update.js

--- update.js
+++ update.js
@@ -15,8 +15,7 @@ async function updateData() {
   function formatDateTime(date) {
     const day = date.getDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const year = date.getFullYear();
-    const hours = date.getHours().toString().padStart(2, "0");
+    const hours = date.getHours().toString();
     const minutes = date.getMinutes().toString().padStart(2, "0");
     return `${day}.${month}. ${hours}:${minutes}`;
   }
