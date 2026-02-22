43b21fbf62cf306a3274327a0c0144e85fbb2051
joeyws <joey.schweickhardt@gmail.com>
2026-02-22 10:11:48 +0100

    Update update.js

--- update.js
+++ update.js
@@ -18,7 +18,7 @@ async function updateData() {
     const year = date.getFullYear();
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
+    return `${day}.${month}. ${hours}:${minutes}`;
   }
 
   // Timestamp
